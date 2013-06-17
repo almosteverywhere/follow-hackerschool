70f64536a8694b638ccfff6e3815a38d7eabe29d
Nina Zakharenko <nzakharenko@gmail.com>
2013-06-17 17:27:58 -0400

    minor changes to css and js

--- app/static/js/closeFlash.js
+++ app/static/js/closeFlash.js
@@ -1,8 +1,4 @@
-;(function () {
-    window.onload = function () {
-        var flash = document.getElementById('flash');
-        window.setTimeout(function () {
-            flash.parentNode.removeChild(flash);
-        }, 2000);
-    }
-})();
+$('#load').on('click', function() {
+    var $this = $(this);
+    $this.button('loading');
+});
