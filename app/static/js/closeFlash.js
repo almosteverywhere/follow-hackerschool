942b0bba3a89e04762b6223f633cb3053555478c
Nina Zakharenko <nzakharenko@gmail.com>
2013-06-17 22:20:44 -0400

    updates to templates

--- app/static/js/closeFlash.js
+++ app/static/js/closeFlash.js
@@ -1,4 +1,8 @@
-$('#load').on('click', function() {
+$('#load').on('click', function () {
     var $this = $(this);
     $this.button('loading');
+});
+
+$(document).ready(function () {
+    $('#preview-tweet').popover({trigger: "hover"});
 });
