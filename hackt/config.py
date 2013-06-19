94c9e702f77bad4c7d1bc3bb5db8d06baf3b608c
Nina Zakharenko <nzakharenko@gmail.com>
2013-06-19 18:09:19 -0400

    more messing with aws

--- hackt/config.py
+++ hackt/config.py
@@ -4,10 +4,21 @@ class Config(object):
     """
     Default configuration, with test values for consumer key & secret
     """
+    if 'RDS_HOSTNAME' in os.environ:
+        DB = {
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+        SQLALCHEMY_DATABASE_URI = 'mysql://' + DB['USER'] + ":" + DB['PASSWORD'] + '@' + DB['HOST'] + ":" + DB['PORT'] + '/' + DB['NAME']
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+
+
     CONSUMER_KEY = os.getenv('CONSUMER_KEY', 'VLFXgP5KwaaOcHwIj1g5w')
     CONSUMER_SECRET = os.getenv('CONSUMER_SECRET', 'ccEpq0ztdpMbJgbj0vAOemIq3f9PJBE0FKtIxMHvl4')
-
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:////tmp/test.db')
 
     DEBUG = os.getenv('DEBUG', False)
     SECRET_KEY = os.getenv('SECRET_KEY', 'development key')
